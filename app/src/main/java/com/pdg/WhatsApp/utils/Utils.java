89bafb45200519bb08fc5cf259fec0d7e1cb3171
Gabriel Cutas <bcutas12@gmail.com>
2023-01-26 09:23:54 +0100

    Comienzo de creacion funcion getDummyData

--- app/src/main/java/com/pdg/WhatsApp/utils/Utils.java
+++ app/src/main/java/com/pdg/WhatsApp/utils/Utils.java
@@ -1,4 +1,17 @@
 package com.pdg.WhatsApp.utils;
 
+import com.pdg.WhatsApp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
+
+    public static List<User> getDummyData() {
+        List<User> list = new ArrayList<User>();
+
+        list.add(new User(""));
+
+        return list;
+    }
 }
