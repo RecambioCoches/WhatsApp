762c7f7239868e657ea6e499c236c26460aef9e1
Gabriel Cutas <bcutas12@gmail.com>
2023-01-27 09:15:03 +0100

    Login

--- app/src/main/java/com/pdg/WhatsApp/activities/InicioDeSesion.java
+++ app/src/main/java/com/pdg/WhatsApp/activities/InicioDeSesion.java
@@ -2,15 +2,61 @@ package com.pdg.WhatsApp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.pdg.WhatsApp.R;
+import com.pdg.WhatsApp.model.User;
+import com.pdg.WhatsApp.utils.Utils;
+
+import io.realm.Realm;
 
 public class InicioDeSesion extends AppCompatActivity {
+    Realm realm;
+
+    EditText txtUsername;
+    EditText txtPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_de_sesion);
+        realm = Realm.getDefaultInstance();
+
+        txtUsername = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPass);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(view -> {
+            User user = realm.where(User.class).equalTo("username", txtUsername.getText().toString()).findFirst();
+            if (user == null) Toast.makeText(this, "Ese usuario no existe", Toast.LENGTH_SHORT).show();
+            else {
+                if (!user.getPassword().equals(txtPassword.getText().toString()))
+                    Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                else {
+                    try {
+                        Intent intent = new Intent(InicioDeSesion.this, MainActivity.class);
+                        intent.putExtra("id", user.getId()); // Le paso el id que tiene que usar luego en la consulta a la BBDD.
+                        txtUsername.setText("");
+                        txtPassword.setText("");
+                        startActivity(intent);
+                    } catch (Exception e){
+                        System.out.println(e.getMessage());
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+        });
+
+        if (realm.isEmpty()){
+            realm.beginTransaction();
+            realm.copyToRealm(Utils.getDummyData());
+            realm.commitTransaction();
+        }
+
     }
 }
