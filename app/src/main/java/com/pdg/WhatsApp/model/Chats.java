bd0f610fb2a79620e2196773dcfd7bcc0bdfe5cf
david <david.garciasalas@gmail.com>
2023-01-27 09:05:55 +0100

    Cambiado ArrayList por RealmList en el modelo

--- app/src/main/java/com/pdg/WhatsApp/model/Chats.java
+++ app/src/main/java/com/pdg/WhatsApp/model/Chats.java
@@ -4,6 +4,7 @@ import com.pdg.WhatsApp.app.MyApplication;
 
 import java.util.ArrayList;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
@@ -12,13 +13,13 @@ public class Chats extends RealmObject {
     private int id;
 
     private String nombreChat;
-    private ArrayList<Mensaje> mensajes;
+    private RealmList<Mensaje> mensajes;
 
 
     public Chats() {
     }
 
-    public Chats(String nombreChat,ArrayList<Mensaje> mensajes) {
+    public Chats(String nombreChat,RealmList<Mensaje> mensajes) {
         this.id = MyApplication.userId.incrementAndGet();
         this.nombreChat = nombreChat;
         this.mensajes = mensajes;
