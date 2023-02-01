dcefa2828187a7107ef5119a851221623e0e2acb
Gabriel Cutas <bcutas12@gmail.com>
2023-02-01 09:24:35 +0100

    Adaptadores de llamada y estado parcialmente terminados

--- app/src/main/java/com/pdg/WhatsApp/adapters/EstadoRecyclerAdapter.java
+++ app/src/main/java/com/pdg/WhatsApp/adapters/EstadoRecyclerAdapter.java
@@ -1,4 +1,68 @@
 package com.pdg.WhatsApp.adapters;
 
-public class EstadoRecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pdg.WhatsApp.R;
+
+import java.util.List;
+
+public class EstadoRecyclerAdapter extends RecyclerView.Adapter<EstadoRecyclerAdapter.EstadosDataHolder>{
+
+
+    private final List<Hobby> estadoList;
+    private final OnItemClickListener itemListener;
+
+    public EstadoAdapter(List<Estado> hobbies, OnItemClickListener itemListener) {
+        this.estadoList = hobbies;
+        this.itemListener = itemListener;
+    }
+
+    @NonNull
+    @Override
+    public EstadosDataHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.hobby_item, parent, false);
+        return new EstadosDataHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EstadosDataHolder holder, int position) {
+        holder.assignData(estadoList.get(position),itemListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return estadoList.size();
+    }
+
+    public static class EstadosDataHolder extends RecyclerView.ViewHolder {
+        TextView type;
+        TextView title;
+        TextView note;
+
+        public EstadosDataHolder(@NonNull View itemView) {
+            super(itemView);
+            type = (TextView) itemView.findViewById(R.id.textoTipo);
+            title = (TextView) itemView.findViewById(R.id.textoTitulo);
+            note = (TextView) itemView.findViewById(R.id.textoNota);
+        }
+
+        public void assignData(Estado estado, OnItemClickListener itemListener){
+
+            this.type.setText(estado.getTipo());
+            this.title.setText(estado.getNombre());
+            this.note.setText(String.valueOf(estado.getNota()));
+
+            itemView.setOnClickListener(view -> itemListener.onItemClick(estado,getAdapterPosition()));
+        }
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(Estado estado, int position);
+    }
 }
--- app/src/main/java/com/pdg/WhatsApp/adapters/LlamadaRecyclerAdapter.java
+++ app/src/main/java/com/pdg/WhatsApp/adapters/LlamadaRecyclerAdapter.java
@@ -1,4 +1,68 @@
 package com.pdg.WhatsApp.adapters;
 
-public class LlamadaRecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pdg.WhatsApp.R;
+
+import java.util.List;
+
+public class LlamadaRecyclerAdapter extends RecyclerView.Adapter<LlamadaRecyclerAdapter.LlamadasDataHolder>{
+
+    private final List<Llamada> llamadaList;
+    private final OnItemClickListener itemListener;
+
+    public LlamadaAdapter(List<Llamada> llamadas, OnItemClickListener itemListener) {
+        this.llamadaList = llamadas;
+        this.itemListener = itemListener;
+    }
+
+    @NonNull
+    @Override
+    public LlamadasDataHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.llamada_item, parent, false);
+        return new LlamadasDataHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LlamadasDataHolder holder, int position) {
+        holder.assignData(llamadaList.get(position),itemListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return llamadaList.size();
+    }
+
+    public static class LlamadasDataHolder extends RecyclerView.ViewHolder {
+        TextView type;
+        TextView title;
+        TextView note;
+
+        public LlamadasDataHolder(@NonNull View itemView) {
+            super(itemView);
+            type = (TextView) itemView.findViewById(R.id.textoTipo);
+            title = (TextView) itemView.findViewById(R.id.textoTitulo);
+            note = (TextView) itemView.findViewById(R.id.textoNota);
+        }
+
+        public void assignData(Llamada llamada, OnItemClickListener itemListener){
+
+            this.type.setText(llamada.getTipo());
+            this.title.setText(llamada.getNombre());
+            this.note.setText(String.valueOf(llamada.getNota()));
+
+            itemView.setOnClickListener(view -> itemListener.onItemClick(llamada,getAdapterPosition()));
+        }
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(Llamada llamada, int position);
+    }
+
 }
